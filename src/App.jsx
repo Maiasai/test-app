565835919b9edce3d2c3257f9a33ca2c5c8e4be5
Mai <mai@MainoMacBook-Pro.local>
2025-07-25 19:19:07 +0900

    “課題3の実装”

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
-import { TopPage } from './TopPage';
-import { ArticlePage } from './articlepage';
+
 import { Layout } from './Layout';
+import { lazy,Suspense }  from 'react';
 
 
 
 // ルーティング定義
 export const App = ()=>{
-  return(
-    <Routes>
-    {/* 親ルートに共通レイアウトを配置 */}
-    <Route path="/" element={<Layout />}>
 
+  return(
+
+    <Routes>
+      {/* 親ルートに共通レイアウトを配置 */}
+      <Route path="/" element={<Layout />}>
       {/* 子ルート（Outletに差し込まれる） */}
       <Route index element={<TopPage />} />
-      <Route path="posts/:id" element={<ArticlePage />} />    
+      <Route path="posts/:id" element={<ArticlePage />} />   
     </Route>
-  </Routes>
+    </Routes>
+
  );
 }
 
